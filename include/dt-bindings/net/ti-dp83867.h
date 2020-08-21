ffc2c97b9dbaa851e758a4896d038681138086e0
Matthias Schiffer <matthias.schiffer@...tq-group.com>
2020-08-21 09:21:45 +0200

    dt-bindings: dp83867: add ti,led-function and ti,led-ctrl properties
    With the TQ-Systems MBa7x (imx7-mba7.dtsi), a user of these properties
    already sneaked in before they were properly specified. Add them to the
    binding docs.
    
    On top of the existing use (requiring to specify the raw register value
    in the DTS), we propose a few convenience macros and defines.
    
    Signed-off-by: Matthias Schiffer <matthias.schiffer@...tq-group.com>

--- include/dt-bindings/net/ti-dp83867.h
+++ include/dt-bindings/net/ti-dp83867.h
@@ -50,4 +50,64 @@
 #define DP83867_CLK_O_SEL_REF_CLK		0xC
 /* Special flag to indicate clock should be off */
 #define DP83867_CLK_O_SEL_OFF			0xFFFFFFFF
+
+/*
+ * Register values and helper macros for ti,led-function and ti,led-ctrl
+ *
+ * Example:
+ *
+ * ti,led-function = <(DP83867_LED0(FUNC_LINK_ACT) |
+ *                     DP83867_LED1(FUNC_LINK_1000))>;
+ * ti,led-ctrl = <(DP83867_LED0(CTRL_ACTIVE_HIGH) |
+ *                 DP83867_LED1(CTRL_ACTIVE_HIGH) |
+ *                 DP83867_LED2(CTRL_FORCE_LOW) |
+ *                 DP83867_LED3(CTRL_FORCE_LOW))>;
+ *
+ * It is recommended to force all unused LED pins to high or low level via
+ * led-ctrl (led-function is ignored in this case). LEDs that are missing from
+ * the configured value will be set to value 0x0 (FUNC_LINK and
+ * CTRL_ACTIVE_LOW).
+ */
+
+/* Link established */
+#define DP83867_LED_FUNC_LINK		0x0
+/* Receive or transmit activity */
+#define DP83867_LED_FUNC_ACT		0x1
+/* Transmit activity */
+#define DP83867_LED_FUNC_ACT_TX		0x2
+/* Receive activity */
+#define DP83867_LED_FUNC_ACT_RX		0x3
+/* Collision detected */
+#define DP83867_LED_FUNC_COLLISION	0x4
+/* 1000BT link established */
+#define DP83867_LED_FUNC_LINK_1000	0x5
+/* 100BTX link established */
+#define DP83867_LED_FUNC_LINK_100	0x6
+/* 10BT link established */
+#define DP83867_LED_FUNC_LINK_10	0x7
+/* 10/100BT link established */
+#define DP83867_LED_FUNC_LINK_10_100	0x8
+/* 100/1000BT link established */
+#define DP83867_LED_FUNC_LINK_100_1000	0x9
+/* Full duplex */
+#define DP83867_LED_FUNC_FULL_DUPLEX	0xa
+/* Link established, blink for transmit or receive activity */
+#define DP83867_LED_FUNC_LINK_ACT	0xb
+/* Receive Error or Transmit Error */
+#define DP83867_LED_FUNC_ERR		0xd
+/* Receive Error */
+#define DP83867_LED_FUNC_ERR_RX		0xe
+
+#define DP83867_LED_CTRL_ACTIVE_HIGH	0x4
+#define DP83867_LED_CTRL_ACTIVE_LOW	0x0
+#define DP83867_LED_CTRL_FORCE_HIGH	0x3
+#define DP83867_LED_CTRL_FORCE_LOW	0x1
+
+#define DP83867_LED_SHIFT(v, s)		((DP83867_LED_##v) << (s))
+
+#define DP83867_LED0(v)			DP83867_LED_SHIFT(v, 0)
+#define DP83867_LED1(v)			DP83867_LED_SHIFT(v, 4)
+#define DP83867_LED2(v)			DP83867_LED_SHIFT(v, 8)
+#define DP83867_LED3(v)			DP83867_LED_SHIFT(v, 12)
+
 #endif
